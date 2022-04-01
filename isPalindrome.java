ed802d2689e153637958c6b256394f68f9675687
confusedstardust <74608709+confusedstardust@users.noreply.github.com>
2022-04-01 13:25:34 +0800

    Create isPalindrome.java

---
+++ isPalindrome.java
@@ -0,0 +1,8 @@
+public boolean isPalindrome(int x) {
+        String str=String.valueOf(x);
+        for (int i=0;i<str.length()/2;i++){
+            if (str.charAt(i)==str.charAt(str.length()-1-i))
+                continue;
+        }
+        return false;
+    }
