fe28f69b63c46c028954ec1e65e352605ac9ff5f
confusedstardust <74608709+confusedstardust@users.noreply.github.com>
2022-04-03 22:11:09 +0800

    nextGreatestLetter.java

---
+++ nextGreatestLetter.java
@@ -0,0 +1,13 @@
+public static char nextGreatestLetter(char[] letters, char target) {
+        Map<Character,Integer> map=new HashMap<>();
+        int i=0;
+        while (i<26){
+            map.put((char)((i++)+97) ,i+1);
+        }
+        i=0;
+        for (;i<letters.length;i++){
+            if (map.get(target)<map.get(letters[i]))
+                return letters[i];
+        }
+        return letters[0];
+    }
